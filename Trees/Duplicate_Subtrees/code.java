783d96cbc788f24832caf1f4db980ad5d33d75ce
Sahista Mehbis Hasan <sahistahasan001@gmail.com>
2025-09-24 17:46:47 +0530

    Create code.java

---
+++ Trees/Duplicate_Subtrees/code.java
@@ -0,0 +1,31 @@
+class Solution {
+      List<TreeNode> res= new ArrayList<>();
+      Map<String,Integer> map = new HashMap<>();
+    public String postorder(TreeNode root){
+       if(root==null){
+        return "#";
+       }
+       StringBuilder curr= new StringBuilder();
+      String left= postorder(root.left);
+      String right = postorder(root.right);
+      curr.append(root.val);
+      curr.append(",");
+      curr.append(left);
+      curr.append(",");
+      curr.append(right);
+      map.put(curr.toString(),map.getOrDefault(curr.toString(),0)+1);
+      if(map.get(curr.toString())==2){
+        res.add(root);
+      }
+      
+      return curr.toString();
+
+
+    }
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+      
+        StringBuilder sb = new StringBuilder();
+        postorder(root);
+        return res;
+    }
+}
